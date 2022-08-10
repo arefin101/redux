e8849994fd5a88bb4ca1e4c93c7420adf4c4ae75
shakib-007 <shakibur.rahman.fahim@gmail.com>
2022-08-10 10:29:10 +0600

    updated

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,12 +1,12 @@
 import {all, fork} from 'redux-saga/effects';
 
 import counterSaga from './counterSaga';
-//import isLogged from './isLoggedSaga';
+import isLogged from './isLoggedSaga';
 
 
 export default function* allSaga() {
     yield all ([
         fork(counterSaga),
-        //fork(isLogged),
+        fork(isLogged),
     ])
 }
